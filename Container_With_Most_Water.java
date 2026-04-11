a9f721880bad6d612cb378d2027d15a05f52afe4
JosenRaju279 <josenraju279@gmail.com>
2026-04-11 13:21:32 +0530

    Container With Most Water

---
+++ Container_With_Most_Water.java
@@ -0,0 +1,22 @@
+public class Container_With_Most_Water {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxWater = 0;
+
+        while (left < right) {
+            int h = Math.min(height[left], height[right]);
+            int width = right - left;
+            int area = h * width;
+
+            maxWater = Math.max(maxWater, area);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxWater;
+    }
+}
