696d9a74051ba1e6415d128d989d6f8100791fb8
josenRaju279 <josenraju279@gmail.com>
2026-04-27 14:45:11 +0530

    Reverse string

---
+++ Reverse_string.java
@@ -0,0 +1,16 @@
+public class Reverse_string {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (right > left) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            right--;
+            left++;
+        }
+    }
+
+}
