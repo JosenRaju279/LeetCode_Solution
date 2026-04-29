1b7853c21f1c209df07014bfbc0aca15e79f01c7
josenRaju279 <josenraju279@gmail.com>
2026-04-29 15:31:39 +0530

    longest substring without repeating characters

---
+++ longest_substring_without_repeating_characters.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+public class longest_substring_without_repeating_characters {
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        int left = 0;
+        int maxLen = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char ch = s.charAt(right);
+
+            if (map.containsKey(ch)) {
+                left = Math.max(left, map.get(ch) + 1);
+            }
+
+            map.put(ch, right);
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
+}
