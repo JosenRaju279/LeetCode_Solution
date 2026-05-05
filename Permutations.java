f64702a03797f7531d837fbb22b333adf782f5e4
josenRaju279 <josenraju279@gmail.com>
2026-05-05 17:43:04 +0530

    Permutations

---
+++ Permutations.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        backtrack(nums, result, new ArrayList<>(), used);
+        return result;
+    }
+
+    private void backtrack(int[] nums, List<List<Integer>> result, List<Integer> current, boolean[] used) {
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i])
+                continue;
+
+            current.add(nums[i]);
+            used[i] = true;
+
+            backtrack(nums, result, current, used);
+
+            current.remove(current.size() - 1);
+            used[i] = false;
+        }
+    }
+}
