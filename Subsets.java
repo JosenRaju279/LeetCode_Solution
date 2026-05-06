e0dbcb2b18be7992ed707cddd815c1e6422cf800
josenRaju279 <josenraju279@gmail.com>
2026-05-06 15:33:29 +0530

    Subsets

---
+++ Subsets.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subsets {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtracking(0, nums, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtracking(int start, int[] nums, List<Integer> current, List<List<Integer>> result) {
+        result.add(new ArrayList(current));
+
+        for (int i = start; i < nums.length; i++) {
+            current.add(nums[i]);
+
+            backtracking(i + 1, nums, current, result);
+
+            current.remove(current.size() - 1);
+        }
+    }
+}
