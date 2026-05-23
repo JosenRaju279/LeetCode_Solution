de8da075e503bb32575cb986c0a1fb473319bf1f
josenRaju279 <josenraju279@gmail.com>
2026-05-23 18:49:05 +0530

    Longest Uncommon Subsequence I

---
+++ Longest_Uncommon_Subsequence_I.java
@@ -0,0 +1,7 @@
+class Longest_Uncommon_Subsequence_I {
+    public int findLUSlength(String a, String b) {
+        if(a.equals(b)) return -1;
+        
+        return Math.max(a.length(), b.length());
+    }
+}
