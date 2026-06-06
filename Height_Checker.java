6f6bf3945879943bd5e44bb3de94e5cbeb9506a1
josenRaju279 <josenraju279@gmail.com>
2026-06-06 22:54:48 +0530

    Height Checker

---
+++ Height_Checker.java
@@ -0,0 +1,24 @@
+public class Height_Checker {
+    public int heightChecker(int[] heights) {
+        int[] count = new int[101];
+
+        for (int h : heights) {
+            count[h]++;
+        }
+
+        int index = 0;
+        int mismatch = 0;
+
+        for (int h = 1; h <= 100; h++) {
+            while (count[h] > 0) {
+                if (heights[index] != h) {
+                    mismatch++;
+                }
+                index++;
+                count[h]--;
+            }
+        }
+
+        return mismatch;
+    }
+}
