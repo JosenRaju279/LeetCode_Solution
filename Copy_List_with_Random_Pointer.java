df1bf81d99b2e8966d3c6b8e08b68bd9548f0ee5
josenRaju279 <josenraju279@gmail.com>
2026-06-16 21:17:30 +0530

    Copy List with Random Pointer

---
+++ Copy_List_with_Random_Pointer.java
@@ -0,0 +1,40 @@
+import org.w3c.dom.Node;
+
+public class Copy_List_with_Random_Pointer {
+    public Node copyRandomList(Node head) {
+        if (head == null)
+            return null;
+
+        Node curr = head;
+        while (curr != null) {
+            Node copy = new Node(curr.val);
+
+            copy.next = curr.next;
+            curr.next = copy;
+
+            curr = copy.next;
+        }
+
+        curr = head;
+        while (curr != null) {
+            if (curr.random != null) {
+                curr.next.random = curr.random.next;
+            }
+            curr = curr.next.next;
+        }
+
+        curr = head;
+        Node copyhead = head.next;
+
+        while (curr != null) {
+            Node copy = curr.next;
+            curr.next = copy.next;
+
+            if (copy.next != null) {
+                copy.next = copy.next.next;
+            }
+            curr = curr.next;
+        }
+        return copyhead;
+    }
+}
