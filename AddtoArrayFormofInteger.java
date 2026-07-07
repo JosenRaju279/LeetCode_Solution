acff983df3b0490b0706de77c03396821062c16b
josenRaju279 <josenraju279@gmail.com>
2026-07-07 22:02:39 +0530

    Add to Array-Form of Integer

---
+++ AddtoArrayFormofInteger.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class AddtoArrayFormofInteger {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        LinkedList<Integer> ans = new LinkedList<>();
+
+        int i = num.length - 1;
+
+        while (i >= 0 || k > 0) {
+            if (i >= 0) {
+                k += num[i];
+                i--;
+            }
+
+            ans.addFirst(k % 10);
+            k /= 10;
+        }
+
+        return ans;
+    }
+}
