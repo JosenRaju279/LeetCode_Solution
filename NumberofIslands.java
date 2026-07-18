7fd8b02036ba01e62c8f499e0bf6572aeb50adea
josenRaju279 <josenraju279@gmail.com>
2026-07-18 22:52:25 +0530

    Number of Islands

---
+++ NumberofIslands.java
@@ -0,0 +1,35 @@
+public class NumberofIslands {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0)
+            return 0;
+
+        int count = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+
+    private void dfs(char[][] grid, int rows, int cols) {
+        int row = grid.length;
+        int col = grid[0].length;
+
+        if (rows < 0 || row <= rows || cols < 0 || col <= cols || grid[rows][cols] == '0')
+            return;
+
+        grid[rows][cols] = '0';
+
+        dfs(grid, rows + 1, cols);
+        dfs(grid, rows - 1, cols);
+        dfs(grid, rows, cols + 1);
+        dfs(grid, rows, cols - 1);
+    }
+}
