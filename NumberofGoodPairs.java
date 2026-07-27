47e79abf193b6b4ee8c9b4be03a624a080327944
josenRaju279 <josenraju279@gmail.com>
2026-07-27 19:27:58 +0530

    Number of Good Pairs

---
+++ NumberofGoodPairs.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+public class NumberofGoodPairs {
+    public int numIdenticalPairs(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+
+        for (int num : nums) {
+            count += map.getOrDefault(num, 0);
+
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        return count;
+    }
+}
