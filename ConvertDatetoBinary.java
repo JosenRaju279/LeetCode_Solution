8fee5fd158de5fc185376d54033f159d9487cf0e
josenRaju279 <josenraju279@gmail.com>
2026-08-01 13:03:41 +0530

    Convert Date to Binary

---
+++ ConvertDatetoBinary.java
@@ -0,0 +1,11 @@
+public class ConvertDatetoBinary {
+    public String convertDateToBinary(String date) {
+        String[] parts = date.split("-");
+
+        String year = Integer.toBinaryString(Integer.parseInt(parts[0]));
+        String month = Integer.toBinaryString(Integer.parseInt(parts[1]));
+        String day = Integer.toBinaryString(Integer.parseInt(parts[2]));
+
+        return year + "-" + month + "-" + day;
+    }
+}
