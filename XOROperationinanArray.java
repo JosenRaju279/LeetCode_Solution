6bf9fb01225520cb4927d488267de418f2249cd0
josenRaju279 <josenraju279@gmail.com>
2026-08-06 18:13:26 +0530

    XOR Operation in an Array

---
+++ XOROperationinanArray.java
@@ -0,0 +1,11 @@
+public class XOROperationinanArray {
+    public int xorOperation(int n, int start) {
+        int xor = 0;
+
+        for (int i = 0; i < n; i++) {
+            xor ^= (start + 2 * i);
+        }
+
+        return xor;
+    }
+}
