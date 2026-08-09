dce2b0bf3a23f2a1ea9c8a6a4842cad4d5c1ed1c
josenRaju279 <josenraju279@gmail.com>
2026-08-09 13:43:14 +0530

    Restore Finishing Order

---
+++ RestoreFinishingOrder.java
@@ -0,0 +1,15 @@
+public class RestoreFinishingOrder {
+    public int[] recoverOrder(int[] order, int[] friends) {
+        int[] ans = new int[friends.length];
+        int index = 0;
+
+        for (int i = 0; i < order.length; i++) {
+            for (int j = 0; j < friends.length; j++) {
+                if (order[i] == friends[j]) {
+                    ans[index++] = order[i];
+                }
+            }
+        }
+        return ans;
+    }
+}
