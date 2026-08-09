c8e9d33ca247ebafd948dc4c822f7edc129d3e5b
josenRaju279 <josenraju279@gmail.com>
2026-08-09 13:30:45 +0530

    Concatenate Array With Reverse

---
+++ ConcatenateArrayWithReverse.java
@@ -0,0 +1,17 @@
+public class ConcatenateArrayWithReverse {
+    public int[] concatWithReverse(int[] nums) {
+        int[] ans = new int[nums.length * 2];
+
+        int left = 0;
+        int right = ans.length - 1;
+
+        while (left < nums.length) {
+            ans[left] = nums[left];
+            ans[right] = nums[left];
+
+            left++;
+            right--;
+        }
+        return ans;
+    }
+}
