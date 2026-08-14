a0738059966f01053b3b102c22f5552c302ce8ac
josenRaju279 <josenraju279@gmail.com>
2026-08-14 18:26:22 +0530

    Count Partitions with Even Sum Difference

---
+++ CountPartitionswithEvenSumDifference.java
@@ -0,0 +1,11 @@
+public class CountPartitionswithEvenSumDifference {
+    public int countPartitions(int[] nums) {
+        int sum = 0;
+
+        for (int num : nums) {
+            sum += num;
+        }
+
+        return (sum % 2 == 0) ? nums.length - 1 : 0;
+    }
+}
