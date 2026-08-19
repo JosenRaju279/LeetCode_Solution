44ccb780f7f82987df91a6335448a9c347fff7ba
josenRaju279 <josenraju279@gmail.com>
2026-08-19 13:37:19 +0530

    Subarray Sum Equals K

---
+++ SubarraySumEqualsK.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubarraySumEqualsK {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1);
+
+        int prefixsum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            prefixsum += num;
+
+            count += map.getOrDefault(prefixsum - k, 0);
+            map.put(prefixsum, map.getOrDefault(prefixsum, 0) + 1);
+        }
+        return count;
+    }
+}
