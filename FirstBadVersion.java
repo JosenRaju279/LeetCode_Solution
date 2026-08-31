be936d8cdfa0704cba2fc6aaab8f2d54e4db0d6f
josenRaju279 <josenraju279@gmail.com>
2026-08-31 10:23:25 +0530

    First Bad Version

---
+++ FirstBadVersion.java
@@ -0,0 +1,17 @@
+public class FirstBadVersion {
+    public int firstBadVersion(int n) {
+        int left = 0;
+        int right = n;
+
+        while (right > left) {
+            int mid = left + (right - left) / 2;
+
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
