d14fbd99581e8ae7484561a17151014d53d2e0ae
josenRaju279 <josenraju279@gmail.com>
2026-09-03 13:52:13 +0530

    Design Linked List

---
+++ DesignLinkedList.java
@@ -0,0 +1,99 @@
+public class DesignLinkedList {
+    private class Node {
+        int val;
+        Node next;
+
+        Node(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+    private int size;
+
+    public MyLinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            return -1;
+        }
+        Node cur = head;
+
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        return cur.val;
+    }
+
+    public void addAtHead(int val) {
+        Node newNode = new Node(val);
+
+        newNode.next = head;
+        head = newNode;
+        size++;
+    }
+
+    public void addAtTail(int val) {
+        Node newNode = new Node(val);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node cur = head;
+
+            while (cur.next != null) {
+                cur = cur.next;
+            }
+            cur.next = newNode;
+        }
+        size++;
+    }
+
+    public void addAtIndex(int index, int val) {
+        if (index < 0 || index > size) {
+            return;
+        }
+
+        if (index == 0) {
+            addAtHead(val);
+        }
+
+        Node newNode = new Node(val);
+        Node cur = head;
+
+        for (int i = 0; i < index - 1; i++) {
+            cur = cur.next;
+        }
+
+        newNode.next = cur.next;
+        cur.next = newNode;
+
+        size++;
+    }
+
+    public void deleteAtIndex(int index) {
+        if (index < 0 || index > size) {
+            return;
+        }
+
+        if (index == 0) {
+            head = head.next;
+            size--;
+            return;
+        }
+
+        Node cur = head;
+
+        for (int i = 0; i < index - 1; i++) {
+            cur = cur.next;
+        }
+
+        cur.next = cur.next.next;
+
+        size--;
+    }
+}
