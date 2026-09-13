24cc9823fa1c01352244802bf2896c487139e037
josenRaju279 <josenraju279@gmail.com>
2026-09-13 14:49:54 +0530

    Binary Tree Inorder Traversal

---
+++ BinaryTreeInorderTraversal.java
@@ -0,0 +1,19 @@
+public class BinaryTreeInorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+            current = stack.pop();
+            result.add(current.val);
+            current = current.right;
+        }
+
+        return result;
+    }
+}
