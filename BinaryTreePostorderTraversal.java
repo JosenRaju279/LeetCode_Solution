f311f957f1f11c8800970f66755535533609c3d3
josenRaju279 <josenraju279@gmail.com>
2026-09-14 17:14:19 +0530

    Binary Tree Postorder Traversal

---
+++ BinaryTreePostorderTraversal.java
@@ -0,0 +1,21 @@
+public class BinaryTreePostorderTraversal {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+
+        postOrder(root, result);
+
+        return result;
+    }
+
+    private void postOrder(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+
+        postOrder(root.left, result);
+
+        postOrder(root.right, result);
+
+        result.add(root.val);
+    }
+}
